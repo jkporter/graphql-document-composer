aa38154bad292253a7ba1ed869a5f867e1968a76
Jonathan Porter <jporter@ingenio.com>
2019-02-08 17:40:47 -0800

    Add validation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const utilities_1 = require("graphql/utilities");
 const language_1 = require("graphql/language");
+const type_1 = require("graphql/type");
 const dependency_graph_1 = require("dependency-graph");
 const fs = require("fs");
 const path = require("path");
@@ -10,46 +11,53 @@ const program = require("commander");
 function directoryWalker(directory, done) {
     let results = [];
     fs.readdir(directory, function (err, list) {
-        if (err)
+        if (err) {
             return done(err);
+        }
         let pending = list.length;
-        if (!pending)
+        if (!pending) {
             return done(null, results);
+        }
         list.forEach(function (file) {
             file = path.resolve(directory, file);
             fs.stat(file, function (err, stat) {
-                // If directory, execute a recursive call
+                // if directory, execute a recursive call
                 if (stat && stat.isDirectory()) {
-                    // Add directory to array [comment if you need to remove the directories from the array]
+                    // add directory to array [comment if you need to remove the directories from the array]
                     results.push(file);
                     directoryWalker(file, function (err, res) {
                         results = results.concat(res);
-                        if (!--pending)
+                        if (!--pending) {
                             done(null, results);
+                        }
                     });
                 }
                 else {
                     results.push(file);
-                    if (!--pending)
+                    if (!--pending) {
                         done(null, results);
+                    }
                 }
             });
         });
     });
 }
 function unwrap(typeNode) {
-    return typeNode.kind === 'NamedType' ? typeNode : unwrap(typeNode.type);
+    return typeNode.kind === "NamedType" ? typeNode : unwrap(typeNode.type);
 }
-function hasExtentionsFor(documentA, documentB) {
+function hasExtensionsFor(documentA, documentB) {
     for (const typeSystemExtensionNode of documentA.definitions.filter(language_1.isTypeSystemExtensionNode)) {
         for (const typeSystemDefinitionNode of documentB.definitions.filter(language_1.isTypeSystemDefinitionNode)) {
             const typeDefinitionMatch = /^(.+)Definition$/.exec(typeSystemDefinitionNode.kind);
             const typeExtensionMatch = /^(.+)Extension$/.exec(typeSystemExtensionNode.kind);
-            if (typeDefinitionMatch[1] === 'Schema' && typeExtensionMatch[1] === 'Schema')
+            if (typeDefinitionMatch[1] === "Schema" && typeExtensionMatch[1] === "Schema") {
                 return true;
+            }
             if (typeDefinitionMatch[1] === typeExtensionMatch[1]
-                && (typeSystemDefinitionNode.name.value === typeSystemExtensionNode.name.value))
+                && (typeSystemDefinitionNode.name.value
+                    === typeSystemExtensionNode.name.value)) {
                 return true;
+            }
         }
     }
     return false;
@@ -58,87 +66,112 @@ function hasTypesFor(documentA, documentB) {
     const typeNames = [];
     const directiveNames = [];
     for (const definition of documentA.definitions) {
-        if (language_1.isTypeDefinitionNode(definition))
+        if (language_1.isTypeDefinitionNode(definition)) {
             directiveNames.push(...definition.directives.map(directive => directive.name.value));
-        if (definition.kind === 'ObjectTypeDefinition' || definition.kind === 'ObjectTypeExtension')
-            typeNames.push(...definition.interfaces.map(graphQLinterface => graphQLinterface.name.value));
-        if (definition.kind === 'ObjectTypeDefinition'
-            || definition.kind === 'InterfaceTypeDefinition'
-            || definition.kind === 'InputObjectTypeDefinition'
-            || definition.kind === 'ObjectTypeExtension'
-            || definition.kind === 'InterfaceTypeExtension'
-            || definition.kind === 'InputObjectTypeExtension')
+        }
+        if (definition.kind === "ObjectTypeDefinition" || definition.kind === "ObjectTypeExtension") {
+            typeNames.push(...definition.interfaces.map(graphQLInterface => graphQLInterface.name.value));
+        }
+        if (definition.kind === "ObjectTypeDefinition"
+            || definition.kind === "InterfaceTypeDefinition"
+            || definition.kind === "InputObjectTypeDefinition"
+            || definition.kind === "ObjectTypeExtension"
+            || definition.kind === "InterfaceTypeExtension"
+            || definition.kind === "InputObjectTypeExtension") {
             for (const field of definition.fields) {
-                if (field.kind === 'FieldDefinition')
+                if (field.kind === "FieldDefinition") {
                     typeNames.push(...field.arguments.map(argument => unwrap(argument.type).name.value));
+                }
                 typeNames.push(unwrap(field.type).name.value);
                 directiveNames.push(...field.directives.map(directive => directive.name.value));
             }
-        if (definition.kind === 'UnionTypeDefinition' || definition.kind === 'UnionTypeExtension')
+        }
+        if (definition.kind === "UnionTypeDefinition" || definition.kind === "UnionTypeExtension") {
             typeNames.push(...definition.types.map(type => unwrap(type).name.value));
-        if (definition.kind === 'EnumTypeDefinition' || definition.kind === 'EnumTypeExtension')
+        }
+        if (definition.kind === "EnumTypeDefinition" || definition.kind === "EnumTypeExtension") {
             directiveNames.push(...definition.values.reduce((names, value) => names.concat(value.directives.map(directive => directive.name.value)), []));
+        }
     }
     if (documentB.definitions
-        .filter(definition => definition.kind === 'DirectiveDefinition')
-        .some(directive => directiveNames.findIndex(name => name === directive.name.value) > -1))
+        .filter(definition => definition.kind === "DirectiveDefinition")
+        .some(directive => directiveNames.findIndex(name => name === directive.name.value) > -1)) {
         return true;
+    }
     if (documentB.definitions
         .filter(language_1.isTypeDefinitionNode)
-        .some(type => typeNames.findIndex(name => name === type.name.value) > -1))
+        .some(type => typeNames.findIndex(name => name === type.name.value) > -1)) {
         return true;
+    }
     return false;
 }
 function isDependentOn(documentA, documentB) {
-    return hasExtentionsFor(documentA, documentB) || hasTypesFor(documentA, documentB);
+    return hasExtensionsFor(documentA, documentB) || hasTypesFor(documentA, documentB);
 }
+const fileFilter = (fileName) => /^.*\.g(raph)?ql$/i.test(fileName);
 function build(dir, output) {
     directoryWalker(dir, function (err, data) {
-        if (err)
+        if (err) {
             throw err;
+        }
         const sources = data.filter(fileFilter).map((fileName, index) => {
-            const body = fs.readFileSync(fileName, 'utf8');
+            const body = fs.readFileSync(fileName, "utf8");
             return new language_1.Source(body, path.relative(dir, fileName));
         });
         const graph = new dependency_graph_1.DepGraph();
-        const documents = sources.map(source => language_1.parse(source));
+        const documents = sources.map((source) => language_1.parse(source));
         documents.forEach(document => {
             console.log(`Adding ${document.loc.source.name}`);
             graph.addNode(document.loc.source.name, document);
         });
         documents.forEach(documentA => {
-            documents.forEach(documentB => {
-                if (documentA.loc.source.name !== documentB.loc.source.name && isDependentOn(documentA, documentB)) {
-                    console.log(`"${documentA.loc.source.name}" has a dependencey on "${documentB.loc.source.name}".`);
-                    graph.addDependency(documentA.loc.source.name, documentB.loc.source.name);
-                }
+            documents.filter(documentB => documentB.loc.source.name !== documentA.loc.source.name && isDependentOn(documentA, documentB))
+                .forEach(documentB => {
+                console.log(`"${documentA.loc.source.name}" has a dependency on "${documentB.loc.source.name}".`);
+                graph.addDependency(documentA.loc.source.name, documentB.loc.source.name);
             });
         });
         const schema = graph.overallOrder()
             .map(nodeName => graph.getNodeData(nodeName))
             .reduce((schema, document, index) => {
-            if (index === 0)
-                console.log(`Budiling schema with ${document.loc.source.name}.`);
-            else
+            if (index === 0) {
+                console.log(`Building schema with ${document.loc.source.name}.`);
+            }
+            else {
                 console.log(`Extending with ${document.loc.source.name}.`);
-            return index == 0 ? utilities_1.buildASTSchema(document) : utilities_1.extendSchema(schema, document);
+            }
+            return index === 0 ? utilities_1.buildASTSchema(document) : utilities_1.extendSchema(schema, document);
         }, null);
-        fs.mkdirSync(path.dirname(output), { recursive: true });
+        console.log();
+        console.log("Validating...");
+        const validationErrors = type_1.validateSchema(schema);
+        if (validationErrors.length) {
+            console.log();
+            validationErrors.forEach(value => {
+                console.error(value.message);
+            });
+            console.error("Validation Failed!");
+            process.exit(1);
+        }
+        console.log("Saving " + path.relative(".", output));
+        if (!fs.existsSync(output)) {
+            fs.mkdirSync(path.dirname(output), { recursive: true });
+        }
         fs.writeFileSync(output, utilities_1.printSchema(schema));
     });
 }
-const fileFilter = (fileName) => /^.*\.g(raph)?ql$/i.test(fileName);
 program
-    .version('1.0.0', '-v, --version')
-    .option('-s, --source <sourceDirectory>', 'Source directory.')
-    .option('-o, --output <outputFilename>', 'Output filename.')
-    .option('-w, --watch', 'Watch for changes and re-build.')
+    .version("1.0.0", "-v, --version")
+    .option("-s, --source <sourceDirectory>", "Source directory.")
+    .option("-o, --output <outputFilename>", "Output filename.")
+    .option("-w, --watch", "Watch for changes and re-build.")
     .parse(process.argv);
 const start = () => build(program.source, program.output);
 if (program.watch) {
     fs.watch(program.source, { recursive: true }, (eventType, filename) => {
-        if (fileFilter(filename))
+        if (fileFilter(filename)) {
             return;
+        }
         start();
     });
 }
--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 ﻿#!/usr/bin/env node
-import { extendSchema, buildASTSchema, printSchema } from 'graphql/utilities'
+import { extendSchema, buildASTSchema, printSchema } from "graphql/utilities";
 import {
     parse,
     Source,
@@ -12,40 +12,41 @@ import {
     isTypeDefinitionNode,
     TypeNode,
     NamedTypeNode
-} from 'graphql/language';
-import { GraphQLSchema } from 'graphql/type';
-import { DepGraph } from 'dependency-graph';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as program from 'commander';
+} from "graphql/language";
+import { GraphQLSchema, validateSchema } from "graphql/type";
+import { DepGraph } from "dependency-graph";
+import * as fs from "fs";
+import * as path from "path";
+import * as program from "commander";
+import { GraphQLError } from "graphql";
 
-function directoryWalker(directory: string, done: (err: NodeJS.ErrnoException, list?: string[]) => void) {
-    let results = [];
+function directoryWalker(directory: string, done: (err: NodeJS.ErrnoException, list?: string[]) => void): void {
+    let results: string[] = [];
 
-    fs.readdir(directory, function (err, list) {
-        if (err) return done(err);
+    fs.readdir(directory, function (err: NodeJS.ErrnoException, list: string[]): void {
+        if (err) { return done(err); }
 
-        let pending = list.length;
+        let pending: number = list.length;
 
-        if (!pending) return done(null, results);
+        if (!pending) { return done(null, results); }
 
-        list.forEach(function (file) {
+        list.forEach(function (file: string): void {
             file = path.resolve(directory, file);
 
-            fs.stat(file, function (err, stat) {
-                // If directory, execute a recursive call
+            fs.stat(file, function (err: NodeJS.ErrnoException, stat: fs.Stats): void {
+                // if directory, execute a recursive call
                 if (stat && stat.isDirectory()) {
-                    // Add directory to array [comment if you need to remove the directories from the array]
+                    // add directory to array [comment if you need to remove the directories from the array]
                     results.push(file);
 
-                    directoryWalker(file, function (err, res) {
+                    directoryWalker(file, function (err: NodeJS.ErrnoException, res: string[]): void {
                         results = results.concat(res);
-                        if (!--pending) done(null, results);
+                        if (!--pending) { done(null, results); }
                     });
                 } else {
                     results.push(file);
 
-                    if (!--pending) done(null, results);
+                    if (!--pending) { done(null, results); }
                 }
             });
         });
@@ -53,132 +54,159 @@ function directoryWalker(directory: string, done: (err: NodeJS.ErrnoException, l
 }
 
 function unwrap(typeNode: TypeNode): NamedTypeNode {
-    return typeNode.kind === 'NamedType' ? typeNode : unwrap(typeNode.type);
+    return typeNode.kind === "NamedType" ? typeNode : unwrap(typeNode.type);
 }
 
-function hasExtentionsFor(documentA: DocumentNode, documentB: DocumentNode): boolean {
+function hasExtensionsFor(documentA: DocumentNode, documentB: DocumentNode): boolean {
     for (const typeSystemExtensionNode of documentA.definitions.filter(isTypeSystemExtensionNode)) {
         for (const typeSystemDefinitionNode of documentB.definitions.filter(isTypeSystemDefinitionNode)) {
-            const typeDefinitionMatch = /^(.+)Definition$/.exec(typeSystemDefinitionNode.kind);
-            const typeExtensionMatch = /^(.+)Extension$/.exec(typeSystemExtensionNode.kind);
+            const typeDefinitionMatch: RegExpExecArray = /^(.+)Definition$/.exec(typeSystemDefinitionNode.kind);
+            const typeExtensionMatch: RegExpExecArray = /^(.+)Extension$/.exec(typeSystemExtensionNode.kind);
 
-            if (typeDefinitionMatch[1] === 'Schema' && typeExtensionMatch[1] === 'Schema')
+            if (typeDefinitionMatch[1] === "Schema" && typeExtensionMatch[1] === "Schema") {
                 return true;
+            }
 
             if (typeDefinitionMatch[1] === typeExtensionMatch[1]
-                && ((typeSystemDefinitionNode as TypeDefinitionNode | DirectiveDefinitionNode).name.value === (typeSystemExtensionNode as TypeExtensionNode).name.value))
+                && ((typeSystemDefinitionNode as TypeDefinitionNode | DirectiveDefinitionNode).name.value
+                    === (typeSystemExtensionNode as TypeExtensionNode).name.value)) {
                 return true;
+            }
         }
     }
     return false;
 }
 
-function hasTypesFor(documentA: DocumentNode, documentB: DocumentNode) {
+function hasTypesFor(documentA: DocumentNode, documentB: DocumentNode): boolean {
     const typeNames: string[] = [];
     const directiveNames: string[] = [];
 
     for (const definition of documentA.definitions) {
-        if (isTypeDefinitionNode(definition))
-            directiveNames.push(...definition.directives.map(directive => directive.name.value))
+        if (isTypeDefinitionNode(definition)) {
+            directiveNames.push(...definition.directives.map(directive => directive.name.value));
+        }
 
-        if (definition.kind === 'ObjectTypeDefinition' || definition.kind === 'ObjectTypeExtension')
-            typeNames.push(...definition.interfaces.map(graphQLinterface => graphQLinterface.name.value));
+        if (definition.kind === "ObjectTypeDefinition" || definition.kind === "ObjectTypeExtension") {
+            typeNames.push(...definition.interfaces.map(graphQLInterface => graphQLInterface.name.value));
+        }
 
-        if (definition.kind === 'ObjectTypeDefinition'
-            || definition.kind === 'InterfaceTypeDefinition'
-            || definition.kind === 'InputObjectTypeDefinition'
-            || definition.kind === 'ObjectTypeExtension'
-            || definition.kind === 'InterfaceTypeExtension'
-            || definition.kind === 'InputObjectTypeExtension')
+        if (definition.kind === "ObjectTypeDefinition"
+            || definition.kind === "InterfaceTypeDefinition"
+            || definition.kind === "InputObjectTypeDefinition"
+            || definition.kind === "ObjectTypeExtension"
+            || definition.kind === "InterfaceTypeExtension"
+            || definition.kind === "InputObjectTypeExtension") {
             for (const field of definition.fields) {
-                if (field.kind === 'FieldDefinition')
+                if (field.kind === "FieldDefinition") {
                     typeNames.push(...field.arguments.map(argument => unwrap(argument.type).name.value));
+                }
                 typeNames.push(unwrap(field.type).name.value);
-                directiveNames.push(...field.directives.map(directive => directive.name.value))
+                directiveNames.push(...field.directives.map(directive => directive.name.value));
             }
+        }
 
-        if (definition.kind === 'UnionTypeDefinition' || definition.kind === 'UnionTypeExtension')
+        if (definition.kind === "UnionTypeDefinition" || definition.kind === "UnionTypeExtension") {
             typeNames.push(...definition.types.map(type => unwrap(type).name.value));
+        }
 
-        if (definition.kind === 'EnumTypeDefinition' || definition.kind === 'EnumTypeExtension')
-            directiveNames.push(...definition.values.reduce((names, value) => names.concat(value.directives.map(directive => directive.name.value)), []));
+        if (definition.kind === "EnumTypeDefinition" || definition.kind === "EnumTypeExtension") {
+            directiveNames.push(...definition.values.reduce((names, value) =>
+                names.concat(value.directives.map(directive => directive.name.value)), []));
+        }
     }
 
     if (documentB.definitions
-        .filter(definition => definition.kind === 'DirectiveDefinition')
-        .some(directive => directiveNames.findIndex(name => name === (directive as DirectiveDefinitionNode).name.value) > -1))
+        .filter(definition => definition.kind === "DirectiveDefinition")
+        .some(directive => directiveNames.findIndex(name => name === (directive as DirectiveDefinitionNode).name.value) > -1)) {
         return true;
+    }
 
     if (documentB.definitions
         .filter(isTypeDefinitionNode)
-        .some(type => typeNames.findIndex(name => name === type.name.value) > -1))
+        .some(type => typeNames.findIndex(name => name === type.name.value) > -1)) {
         return true;
+    }
 
     return false;
 }
 
-function isDependentOn(documentA: DocumentNode, documentB: DocumentNode) {
-    return hasExtentionsFor(documentA, documentB) || hasTypesFor(documentA, documentB)
+function isDependentOn(documentA: DocumentNode, documentB: DocumentNode): boolean {
+    return hasExtensionsFor(documentA, documentB) || hasTypesFor(documentA, documentB);
 }
 
-function build(dir: string, output: string) {
-    directoryWalker(dir, function (err: any, data) {
-        if (err)
-            throw err;
+const fileFilter: (fileName: string) => boolean = (fileName: string) => /^.*\.g(raph)?ql$/i.test(fileName);
 
-        const sources = data.filter(fileFilter).map((fileName, index) => {
-            const body = fs.readFileSync(fileName, 'utf8');
+function build(dir: string, output: string): void {
+    directoryWalker(dir, function (err: NodeJS.ErrnoException, data: string[]): void {
+        if (err) {
+            throw err;
+        }
+
+        const sources: Source[] = data.filter(fileFilter).map((fileName, index) => {
+            const body: string = fs.readFileSync(fileName, "utf8");
             return new Source(body, path.relative(dir, fileName));
         });
 
-        const graph = new DepGraph();
-        const documents : DocumentNode[] = sources.map((source: string | Source) => parse(source));
+        const graph: DepGraph<DocumentNode> = new DepGraph();
+        const documents: DocumentNode[] = sources.map((source: string | Source) => parse(source));
         documents.forEach(document => {
             console.log(`Adding ${document.loc.source.name}`);
             graph.addNode(document.loc.source.name, document);
         });
 
         documents.forEach(documentA => {
-            documents.forEach(documentB => {
-                if (documentA.loc.source.name !== documentB.loc.source.name && isDependentOn(documentA, documentB)) {
-                    console.log(`"${documentA.loc.source.name}" has a dependencey on "${documentB.loc.source.name}".`);
+            documents.filter(documentB => documentB.loc.source.name !== documentA.loc.source.name && isDependentOn(documentA, documentB))
+                .forEach(documentB => {
+                    console.log(`"${documentA.loc.source.name}" has a dependency on "${documentB.loc.source.name}".`);
                     graph.addDependency(documentA.loc.source.name, documentB.loc.source.name);
-                }
-            });
+                });
         });
 
-        const schema = graph.overallOrder()
+        const schema: GraphQLSchema = graph.overallOrder()
             .map(nodeName => graph.getNodeData(nodeName))
             .reduce<GraphQLSchema>(
                 (schema, document: DocumentNode, index) => {
-                    if (index === 0)
-                        console.log(`Budiling schema with ${document.loc.source.name}.`);
-                    else
+                    if (index === 0) {
+                        console.log(`Building schema with ${document.loc.source.name}.`);
+                    } else {
                         console.log(`Extending with ${document.loc.source.name}.`);
-                    return index == 0 ? buildASTSchema(document) : extendSchema(schema, document)
+                    }
+                    return index === 0 ? buildASTSchema(document) : extendSchema(schema, document);
                 },
                 null);
 
-        fs.mkdirSync(path.dirname(output), { recursive: true });
+        console.log();
+        console.log("Validating...");
+        const validationErrors: ReadonlyArray<GraphQLError> = validateSchema(schema);
+        if (validationErrors.length) {
+            console.log();
+            validationErrors.forEach(value => {
+                console.error(value.message);
+            });
+            console.error("Validation Failed!");
+            process.exit(1);
+        }
+
+        console.log("Saving " + path.relative(".", output));
+        if (!fs.existsSync(output)) { fs.mkdirSync(path.dirname(output), { recursive: true }); }
         fs.writeFileSync(output, printSchema(schema));
     });
 }
 
-const fileFilter = (fileName: string) => /^.*\.g(raph)?ql$/i.test(fileName);
-
 program
-    .version('1.0.0', '-v, --version')
-    .option('-s, --source <sourceDirectory>', 'Source directory.')
-    .option('-o, --output <outputFilename>', 'Output filename.')
-    .option('-w, --watch', 'Watch for changes and re-build.')
+    .version("1.0.0", "-v, --version")
+    .option("-s, --source <sourceDirectory>", "Source directory.")
+    .option("-o, --output <outputFilename>", "Output filename.")
+    .option("-w, --watch", "Watch for changes and re-build.")
     .parse(process.argv);
 
-const start = () => build(program.source, program.output);
+const start: () => void = () => build(program.source, program.output);
 
 if (program.watch) {
     fs.watch(program.source, { recursive: true }, (eventType, filename) => {
-        if (fileFilter(filename))
+        if (fileFilter(filename)) {
             return;
+        }
         start();
     });
 }
